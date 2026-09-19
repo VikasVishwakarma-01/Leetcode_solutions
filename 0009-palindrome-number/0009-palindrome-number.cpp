66e0ff5a1569927957e30a8dca48c25d26df0f25
Vikas Vishwakarma <vikasweb77@gmail.com>
2026-09-20 00:00:40 +0530

    Time: 9 ms (6.63%), Space: 11.1 MB (7.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        if(x>-1 && x<10) return true;
-        vector<int>ans;
-        int digit = 0;
-        while(x>0){
-            digit = x%10;
-            ans.push_back(digit);
-            x = x/10;
-        }
-        int n = ans.size();
-        for(int i=0;i<ans.size()/2;i++){
-            if(ans[i]!=ans[n-1-i]){
+        if(x < 0) return false;
+
+        string s = to_string(x);
+        int n = s.length();
+
+        for(int i = 0; i < n/2; i++) {
+            if(s[i] != s[n - 1 - i])
                 return false;
-            }
         }
+
         return true;
     }
 };
